668f00a710a5d54d567891f9f416f70269c6badd
Nurjahan Akter <it23045@mbstu.ac.bd>
2024-11-19 11:39:57 +0600

    Add files via upload

---
+++ Ex.java
@@ -0,0 +1,11 @@
+public class Ex {
+    
+        //Main Method
+        public static void main(String[] args) {
+          /** Display output */
+          System.out.println("Welcome to Java");
+          System.out.println("Welcome to Computer Science");
+          System.out.println("Programming is Fun");
+        } 
+     }
+
